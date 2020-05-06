ec63eb24ebe5955d95398c19ee76c3f50c8137a9
Florian Lenz <33814851+flonairlenz@users.noreply.github.com>
2020-05-06 16:58:45 +0200

    add unit tests

---
+++ src/xStateMachine.Tests/StateMachineTests.cs
@@ -0,0 +1,79 @@
+using xStateMachine.Exceptions;
+using Xunit;
+
+namespace xStateMachine.Tests
+{
+    public class StateMachineTests
+    {
+        [Fact]
+        public void Ctor_SuccessfullyCreateStateMachine_CurrentStateIsEqualToInitalState()
+        {
+            // Arrange
+            var stateMachine = StateMachineBuilder.Create(State.Start);
+
+            // Act    
+            var currentState = stateMachine.CurrentState;
+
+            // Assert
+            Assert.Equal(State.Start, currentState);
+        }
+        
+        [Fact]
+        public void IsTransactionAllowedTo_ValidTransaction_ReturnTrue()
+        {
+            // Arrange
+            var stateMachine = StateMachineBuilder.Create(State.Start);
+            stateMachine.AddTransaction(State.Start, State.Working);
+            
+            // Act
+            var transactionAllowed = stateMachine.IsTransactionAllowedTo(State.Working);
+
+            // Assert
+            Assert.True(transactionAllowed);
+        }
+        
+        [Fact]
+        public void IsTransactionAllowedTo_InvalidTransaction_ReturnFalse()
+        {
+            // Arrange
+            var stateMachine = StateMachineBuilder.Create(State.Start);
+            
+            // Act
+            var transactionAllowed = stateMachine.IsTransactionAllowedTo(State.Working);
+
+            // Assert
+            Assert.False(transactionAllowed);
+        }
+        
+        [Fact]
+        public void ChangeState_ValidTransaction_ReturnNewState()
+        {
+            // Arrange
+            var stateMachine = StateMachineBuilder.Create(State.Start);
+            stateMachine.AddTransaction(State.Start, State.Working);
+            
+            // Act
+            var newState = stateMachine.ChangeState(State.Working);
+
+            // Assert
+            Assert.Equal(State.Working, newState);
+        }
+        
+        [Fact]
+        public void ChangeState_InvalidTransaction_ThrowInvalidTransactionException()
+        {
+            // Arrange
+            var stateMachine = StateMachineBuilder.Create(State.Start);
+            stateMachine.AddTransaction(State.Start, State.Working);
+            
+            // Act, Assert
+            Assert.Throws<InvalidTransactionException>(() => stateMachine.ChangeState(State.Start));
+        }
+    }
+
+    enum State
+    {
+        Start,
+        Working
+    }
+}
